7e02f8cb2d9dfc8580d53170081fd7e438cb32e6
Kamil Włodarczyk <kamil.limanowa@gmail.com>
2026-04-16 20:07:00 +0200

    Simplified Chrome options by switching to remote debugging pipe

--- script/baseScript.py
+++ script/baseScript.py
@@ -20,9 +20,7 @@ class BaseScript:
         options.add_argument("start-maximized")
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-search-engine-choice-screen")
-        options.add_argument("--remote-debugging-port=0")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--remote-debugging-pipe")
         if profile:
             options.add_argument(f"--user-data-dir={self.USER_DATA_DIR}")
             options.add_argument(f"--profile-directory={profile}")
